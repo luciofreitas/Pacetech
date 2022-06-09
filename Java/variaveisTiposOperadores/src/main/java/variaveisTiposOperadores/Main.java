9c93f23490b0f9ea98c7ea97d1260b121305c98b
luciofreitas <luciodfp@gmail.com>
2022-06-08 23:20:53 -0300

    .

---
+++ Java/variaveisTiposOperadores/src/main/java/variaveisTiposOperadores/Main.java
@@ -0,0 +1,24 @@
+package variaveisTiposOperadores;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int idade =0;
+        float cotacaoDolar = 5.0f;
+        double cotacaoEuro = 6.0d;
+        char genero = 'M';
+        byte ponto = 0;
+        boolean estaCadastrado = false;
+        
+        String nome = "Esse é um texto de uma variavel String";
+        
+       /* 
+        +
+        -
+        /
+        * 
+        % 
+        */
+    }
+
+}
