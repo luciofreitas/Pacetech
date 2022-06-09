da60504d4386dc23408ff638e1983513422b436d
luciofreitas <luciodfp@gmail.com>
2022-06-09 00:05:55 -0300

    .

---
+++ Java/estruturaCondicionalESCOLHA/src/main/java/estruturaCondicionalESCOLHA/Main.java
@@ -0,0 +1,20 @@
+package estruturaCondicionalESCOLHA;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int codigoProduto = 2;
+        switch (codigoProduto) {
+            case 1:
+
+                break;
+            case 2:
+                
+                break;
+                
+            default:
+        }
+        
+    }
+
+}
---
+++ Java/estruturaCondicionalSE/src/main/java/estruturaCondicionalSE/Main.java
@@ -0,0 +1,19 @@
+package estruturaCondicionalSE;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int media = 7;
+        if (media >= 7) {
+            if (media == 10) {
+                //Aprovado com nota máxima
+            } else {
+                //Aprovado
+            }
+        } else {
+            //Reprovado
+        }
+    }
+
+}
